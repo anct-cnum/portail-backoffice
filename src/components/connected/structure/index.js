444ceb5d4f43a4e9bddce8988f5fff8f5f9548d9
Loic GUILLOIS <contact@loicguillois.fr>
2021-02-01 17:34:14 +0100

    ajout de la connexion API pour la liste des conseillers

--- src/components/connected/structure/index.js
+++ src/components/connected/structure/index.js
@@ -1,10 +1,22 @@
 import { Route, Redirect } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Menu from './Menu';
 import Conseillers from './Conseillers';
 import Informations from './Informations';
+import { conseillerActions } from '../../../actions';
 
 function Structure({ match }) {
+
+  const dispatch = useDispatch();
+
+  const user = useSelector(state => state.authentication.user.user);
+
+  useEffect(() => {
+    dispatch(conseillerActions.getAll()); // TODO filter on user
+  }, []);
+
   return (
     <div className="structure">
       <h3>Portail structure</h3>
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import authenticationReducer from './authenticationReducer';
+import conseillersReducer from './conseillersReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-    authentication: authenticationReducer
+    authentication: authenticationReducer,
+    conseillers: conseillersReducer
 })
 
 export default rootReducer;
