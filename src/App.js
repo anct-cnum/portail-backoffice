3f5ffe8bc5ebefd6205b56daeec6331adf70661c
Sébastien Quirrion <sebastien@fitlab.fr>
2021-02-04 13:53:24 +0100

    design page connexion

--- src/App.js
+++ src/App.js
@@ -13,7 +13,27 @@ require('dotenv').config()
 function App() {
   return (
     <div className="App">
-      <h1>Back Office</h1>
+      <header className="rf-header">
+        <div className="rf-container">
+          <div className="rf-header__body">
+              <div className="rf-header__brand rf-sur-mesure">
+                  <a href="#" title="République française" style={{boxShadow: "none"}}>
+                      <img src="logos/logoRF.svg" width="96" alt="République Française. Liberté Égalité Fraternité." className="header__logo-rf" />
+                  </a>
+              </div>
+              <div className="rf-header__operator logo-conseiller-numerique">
+                  <img src="logos/logo-conseiller-numerique.svg" alt="logo conseiller numérique France Services" />
+              </div>
+              <div className="rf-header__navbar">
+              <div className="rf-service">
+                  <h1>
+                      Votre espace stucture
+                  </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
       <Router history={history}>
         <Switch>
 
