7a9d43bc8dccb47beba1e72152ccdc30bbddae43
Loic GUILLOIS <contact@loicguillois.fr>
2021-02-16 17:31:03 +0100

    Update Home.js

--- src/components/connected/Home.js
+++ src/components/connected/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link, Route, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Admin from './admin/index';
