a7637b2633320628b190a70b82980fab2fe1b49c
Sébastien Quirrion <sebastien@fitlab.fr>
2021-02-25 10:01:46 +0100

    petites modfifs suite recette

--- src/components/connected/structure/Documents.js
+++ src/components/connected/structure/Documents.js
@@ -7,57 +7,57 @@ function Documents() {
       <div className="rf-container-fluid">
         <div className="rf-grid-row">
           <p>
-            <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Contrat%20de%20projet%20type.pdf"
-              className="rf-link" target="blank" title="Télécharger le contrat de projet type">
-                        Télécharger le contrat de projet type (pdf)
+            <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Guide%20de%20l%27employeur%20-%20Public%20-%20V3.pdf"
+              className="rf-link" target="blank" title="Télécharger le guide de l'employeur" style={{ border: '1px solid #e7e7e7' }}>
+                        Télécharger le guide de l&rsquo;employeur (pdf)
             </a>
-            <span className="rf-highlight" style={{ display: 'inline-block' }}>
-              Contrat de travail à durée déterminée
-            </span>
+            <div className="rf-highlight">
+              Détails du dispositif pour la structure accueillante
+            </div>
           </p>
         </div>
         <div className="rf-grid-row">
           <p>
             <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Fiche%20de%20poste%20type.pdf"
-              className="rf-link" target="blank" title="Télécharger la fiche de poste type">
+              className="rf-link" target="blank" title="Télécharger la fiche de poste type" style={{ border: '1px solid #e7e7e7' }}>
                         Télécharger la fiche de poste type (pdf)
             </a>
-            <span className="rf-highlight" style={{ display: 'inline-block' }}>
+            <div className="rf-highlight">
               Fiche avec détails de l&rsquo;offre, des missions et des tâches
-            </span>
-          </p>
-        </div>
-        <div className="rf-grid-row">
-          <p>
-            <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Mod%C3%A8le%20de%20d%C3%A9lib%C3%A9ration%20contrat%20de%20projet.pdf"
-              className="rf-link" target="blank" title="Télécharger le modèle de délibération">
-                        Télécharger le modèle de délibération (pdf)
-            </a>
-            <span className="rf-highlight" style={{ display: 'inline-block' }}>
-              Document pour la création d&rsquo;un poste non permanent
-            </span>
-          </p>
-        </div>
-        <div className="rf-grid-row">
-          <p>
-            <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Guide%20de%20l%27employeur%20-%20Public%20-%20V3.pdf"
-              className="rf-link" target="blank" title="Télécharger le guide de l'employeur">
-                        Télécharger le guide de l&rsquo;employeur (pdf)
-            </a>
-            <span className="rf-highlight" style={{ display: 'inline-block' }}>
-              Détails du dispositif pour la structure accueillante
-            </span>
+            </div>
           </p>
         </div>
         <div className="rf-grid-row">
           <p>
             <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Guide%20d%27entretien.pdf"
-              className="rf-link" target="blank" title="Télécharger le guide d'entretien">
+              className="rf-link" target="blank" title="Télécharger le guide d'entretien" style={{ border: '1px solid #e7e7e7' }}>
                         Télécharger le guide d&rsquo;entretien (pdf)
             </a>
-            <span className="rf-highlight" style={{ display: 'inline-block' }}>
+            <div className="rf-highlight">
               Préconisations pour l&rsquo;entretien du candidat
-            </span>
+            </div>
+          </p>
+        </div>
+        <div className="rf-grid-row">
+          <p>
+            <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Mod%C3%A8le%20de%20d%C3%A9lib%C3%A9ration%20contrat%20de%20projet.pdf"
+              className="rf-link" target="blank" title="Télécharger le modèle de délibération" style={{ border: '1px solid #e7e7e7' }}>
+                        Télécharger le modèle de délibération (pdf)
+            </a>
+            <div className="rf-highlight">
+              Document pour la création d&rsquo;un poste non permanent
+            </div>
+          </p>
+        </div>
+        <div className="rf-grid-row">
+          <p>
+            <a href="https://cellar-c2.services.clever-cloud.com/conseiller-numerique/Contrat%20de%20projet%20type.pdf"
+              className="rf-link" target="blank" title="Télécharger le contrat de projet type" style={{ border: '1px solid #e7e7e7' }}>
+                        Télécharger le contrat de projet type (pdf)
+            </a>
+            <div className="rf-highlight">
+              Contrat de travail à durée déterminée
+            </div>
           </p>
         </div>
       </div>
