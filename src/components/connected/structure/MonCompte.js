adb60923f28a8ebef3003c9a61fc9ef6694ec97b
Ornella Ourfi <ornella93@outlook.fr>
2021-06-08 13:04:26 +0200

    version 1 et 2 , en attente de l'avis de Rhaphael pour choisir quel version on garde

--- src/components/connected/structure/MonCompte.js
+++ src/components/connected/structure/MonCompte.js
@@ -16,10 +16,32 @@ function MonCompte() {
       <h2>
         Mon compte
       </h2>
+      {/* VERSION 1 */}
+      <div className="rf-grid-row">
+        <p>Nom : { structure?.structure?.contact.nom }</p>
+        <span style={{ color: 'blue' }} className="rf-fi-edit-line rf-mt-2w rf-ml-3v " aria-hidden="true"></span>
+      </div>
+      <div className="rf-grid-row">
+        <p>Prénom : { structure?.structure?.contact.prenom }</p>
+        <span style={{ color: 'blue' }} className="rf-fi-edit-line rf-mt-2w rf-ml-3v " aria-hidden="true"></span>
+      </div>
+      <div className="rf-grid-row">
+        <p>Prénom : { structure?.structure?.contact.fonction }</p>
+        <span style={{ color: 'blue' }} className="rf-fi-edit-line rf-mt-2w rf-ml-3v " aria-hidden="true"></span>
+      </div>
+      <div className="rf-grid-row">
+        <p>Téléphone : { structure?.structure?.contact.telephone }</p>
+        <span style={{ color: 'blue' }} className="rf-fi-edit-line rf-mt-2w rf-ml-3v " aria-hidden="true"></span>
+      </div>
+      {/* VERSION 2 */}
       <p>Nom : { structure?.structure?.contact.nom }</p>
       <p>Prénom : { structure?.structure?.contact.prenom }</p>
       <p>Fonction : { structure?.structure?.contact.fonction }</p>
       <p>Téléphone : { structure?.structure?.contact.telephone }</p>
+      <button className="rf-btn">
+            Modifier mes informations
+        <span style={{ color: 'white' }} className="rf-fi-edit-line rf-ml-4v" aria-hidden="true"/>
+      </button>
     </div>
   );
 }
