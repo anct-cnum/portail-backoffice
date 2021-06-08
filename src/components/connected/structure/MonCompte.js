c497940271ad9ec5e879152862e8cebfe7653794
Ornella Ourfi <ornella93@outlook.fr>
2021-06-08 10:23:59 +0200

    ajout des information dans Mon compte , nom + prenom + fonction & le téléphone

--- src/components/connected/structure/MonCompte.js
+++ src/components/connected/structure/MonCompte.js
@@ -6,8 +6,6 @@ import { structureActions } from '../../../actions';
 function MonCompte() {
   const dispatch = useDispatch();
   const structure = useSelector(state => state.structure);
-  console.log('structure:', structure.structure);
-
 
   useEffect(() => {
     dispatch(structureActions.get());
@@ -18,7 +16,22 @@ function MonCompte() {
       <h2>
         Mon compte
       </h2>
-
+      <div>
+        <p>Nom : { structure?.structure?.contact.nom }</p>
+        <span className="fr-fi-edit-line" aria-hidden="true"></span>
+      </div>
+      <div>
+        <p>Prénom : { structure?.structure?.contact.prenom }</p>
+        <span className="fr-fi-edit-line" aria-hidden="true"></span>
+      </div>
+      <div>
+        <p>Prénom : { structure?.structure?.contact.fonction }</p>
+        <span className="fr-fi-edit-line" aria-hidden="true"></span>
+      </div>
+      <div>
+        <p>Téléphone : { structure?.structure?.contact.telephone }</p>
+        <span className="fr-fi-edit-line" aria-hidden="true"></span>
+      </div>
     </div>
   );
 }
