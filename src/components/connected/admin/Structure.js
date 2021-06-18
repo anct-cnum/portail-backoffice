7d9c2e6432610d3403520b5a3aa938389f6b9d14
Ornella Ourfi <ornella93@outlook.fr>
2021-06-18 09:24:57 +0200

    suppression balise en trop + indentation

--- src/components/connected/admin/Structure.js
+++ src/components/connected/admin/Structure.js
@@ -7,23 +7,20 @@ import { Link } from 'react-router-dom';
 function Structure({ structure, currentPage }) {
 
   return (
-    <>
-      <tr>
-        <td>{structure.idPG}</td>
-        <td>{structure.siret !== null ? structure.siret : 'non renseigné' }</td>
-        <td className="capitalizeFirstLetter">{structure.nom}</td>
-        {/* eslint-disable-next-line max-len */}
-        <td>{structure.statut === 'VALIDATION_COSELEC' && structure.dernierCoselec !== null ? structure.dernierCoselec?.avisCoselec : 'en attente de passage'}</td>
-        <td>{dayjs(structure.createdAt).format('DD/MM/YYYY')}</td>
-        <td>{structure.codePostal}</td>
-        <td>        <Link className="rf-btn rf-fi-eye-line rf-btn--icon-left" style={{ boxShadow: 'none' }}
-          to={{
-            pathname: `/structure/${structure._id}`,
-            currentPage: currentPage
-          }}>Détails</Link></td>
-      </tr>
-    </>
-
+    <tr>
+      <td>{structure.idPG}</td>
+      <td>{structure.siret !== null ? structure.siret : 'non renseigné' }</td>
+      <td className="capitalizeFirstLetter">{structure.nom}</td>
+      {/* eslint-disable-next-line max-len */}
+      <td>{structure.statut === 'VALIDATION_COSELEC' && structure.dernierCoselec !== null ? structure.dernierCoselec?.avisCoselec : 'en attente de passage'}</td>
+      <td>{dayjs(structure.createdAt).format('DD/MM/YYYY')}</td>
+      <td>{structure.codePostal}</td>
+      <td>        <Link className="rf-btn rf-fi-eye-line rf-btn--icon-left" style={{ boxShadow: 'none' }}
+        to={{
+          pathname: `/structure/${structure._id}`,
+          currentPage: currentPage
+        }}>Détails</Link></td>
+    </tr>
   );
 }
 
