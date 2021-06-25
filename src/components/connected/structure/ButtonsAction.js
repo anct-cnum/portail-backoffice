283bcb12158208bc311cb44a8291d20b908da48c
morgan <morgan@fitlab.fr>
2021-06-25 11:02:46 +0200

    Déplacement de la modal

--- src/components/connected/structure/ButtonsAction.js
+++ src/components/connected/structure/ButtonsAction.js
@@ -5,6 +5,7 @@ import fr from 'date-fns/locale/fr';
 import { conseillerActions } from '../../../actions/conseiller.actions';
 import { useDispatch } from 'react-redux';
 import moment from 'moment';
+import PopinConfirmationAnnulation from './popins/popinConfirmationAnnulation';
 
 //Print datePicker calendar in FR
 registerLocale('fr', fr);
@@ -100,50 +101,14 @@ function ButtonsAction({ statut, updateStatut, miseEnRelationId, dateRecrutement
           </div>
         }
         {statut === 'recrutee' &&
+        <>
+          <PopinConfirmationAnnulation
+            updateStatut={updateStatut}
+            updateDateRecrutement={updateDateRecrutement}
+            setDateValidee={setDateValidee}
+            toggleModal={toggleModal}>
+          </PopinConfirmationAnnulation>
           <p className="rf-col-3">
-
-            <dialog aria-labelledby="rf-modal-title-modal-1" role="dialog" id="rf-modal-annuler"
-              className="rf-modal">
-              <div className="rf-container--fluid rf-container-md">
-                <div className="rf-grid-row rf-grid-row--center">
-                  <div className="rf-col-12 rf-col-md-6">
-                    <div className="rf-modal__body centrerTexte">
-                      <div className="rf-modal__header">
-                        <button id="modal-annuler-close" className="rf-link--close rf-link" title="Fermer la fenêtre modale"
-                          aria-controls="rf-modal-1" target="_self" onClick={() => {
-                            toggleModal(false);
-                          }}>
-                          Fermer
-                        </button>
-                      </div>
-                      <div className="rf-modal__content important">
-                        <p>
-                          <strong>
-                            Important : Vous êtes sur le point d&rsquo;annuler votre demande de recrutement pour ce candidat.
-                          </strong>
-                        </p>
-                        <p>
-                          <strong>
-                            Êtes-vous sûr de vouloir réaliser cette action ?
-                          </strong>
-                        </p>
-                        <button onClick={() => {
-                          updateStatut('interessee');
-                          updateDateRecrutement(null);
-                          setDateValidee(null);
-                          toggleModal(false);
-                        }}
-                        className="rf-btn rf-btn--secondary rf-fi-close-circle-line rf-btn--icon-left"
-                        title="Annuler le recrutement">
-                        Je valide l&rsquo;annulation du recrutement
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </dialog>
-
             <button id="btn-annuler" onClick={() => {
               toggleModal(true);
             }}
@@ -152,6 +117,7 @@ function ButtonsAction({ statut, updateStatut, miseEnRelationId, dateRecrutement
             Annuler le recrutement
             </button>
           </p>
+        </>
         }
         {statut === 'finalisee' &&
           <p><strong>Recrutement finalisé pour ce candidat</strong></p>
