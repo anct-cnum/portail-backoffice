a56429a3e56b3cacf996855e78b7598ef388be4a
Ornella Ourfi <ornella93@outlook.fr>
2021-08-04 15:43:07 +0200

    création d'un fichier css pour bien positionner la partie droite du header quand c'est l'espace prefet qui se connecte

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { statsActions } from '../../actions';
+import './Header.css';
 
 function Header({ connected }) {
   const dispatch = useDispatch();
@@ -10,9 +11,10 @@ function Header({ connected }) {
   const totalConseillers = useSelector(state => state?.stats?.totalConseillers);
   const location = useLocation();
   const role = new URLSearchParams(location.search).get('role') ? new URLSearchParams(location.search).get('role') : user?.role;
+  console.log('role:', role);
   const [menu, setmenu] = useState(false);
 
-  if (connected) {
+  if (connected && (role !== 'prefet')) {
     useEffect(() => {
       if (totalConseillers === undefined) {
         dispatch(statsActions.getConseillersFinalisee());
@@ -43,7 +45,8 @@ function Header({ connected }) {
                   <div className="rf-header__navbar">
                     <div className="rf-service">
                       <a className="rf-service__title titrage" href="/" title="Portail Conseiller Numérique">
-                    Espace Structures Conseiller Numérique France Services
+                        {connected ? `Espace ${role === 'structure' ? 'Structure' : 'Prefet'} Conseiller Numérique France Services` :
+                          'Votre espace Conseiller Numérique France Services' }
                       </a>
                       { role === 'structure' &&
                     <p className="rf-service__tagline">Espace de gestion des candidatures</p>
@@ -57,15 +60,18 @@ function Header({ connected }) {
               </div>
               <div className="rf-col" style={{ textAlign: 'right' }}>
                 { connected &&
-                  <div className="rf-header__tools" style={{ marginBottom: '33px', display: 'flex' }}>
+                  <div className="rf-header__tools headerBottom">
                     <div className="rf-shortcuts">
-                      <div className="rf-header rf-mb-2v" style={{ display: 'flex', alignItems: 'center', width: '98%' }}>
-                        <img className="rf-col-xl-2" src="/logos/conseiller-conseillere.svg"
-                          alt="logo conseiller / conseillere de Conseiller Numérique France Services" style={{ height: '32px', paddingLeft: '1.5rem' }}/>
-                        <p className="rf-mt-5v">
-                        &nbsp;<strong>{totalConseillers}</strong> conseillers déjà recrutés dans le dispositif
-                        </p>
-                      </div>
+                      {role !== 'prefet' &&
+                        <div className="rf-header rf-mb-2v nombreTotalConseiller">
+                          <img className="rf-col-xl-2 logoConseiller" src="/logos/conseiller-conseillere.svg"
+                            alt="logo conseiller / conseillere de Conseiller Numérique France Services logoConseiller"/>
+                          <p className="rf-mt-5v">
+                      &nbsp;<strong>{totalConseillers}</strong> conseillers déjà recrutés dans le dispositif
+                          </p>
+                        </div>
+                      }
+                      { role === 'prefet' && <div className="positionBloc"/>}
                       <ul className="rf-shortcuts__list">
                         <li className="rf-shortcuts__item">
                           <span>
@@ -100,6 +106,65 @@ function Header({ connected }) {
         </div>
       </div>
     </header>
+    // <header className="rf-header" role="banner">
+    //   <div className="rf-container">
+    //     <div className="rf-grid-row rf-grid-row--top">
+    //       <div className="rf-col-1"></div>
+    //       <div className="rf-col-10">
+    //         <div className="rf-header__body">
+    //           <a className="rf-header__operator" href="/" style={{ boxShadow: 'none' }}>
+    //             <img src="/logos/logo-conseiller-numerique.svg" alt="logo Conseiller Numérique France Services" style={{ height: '50px' }}/>
+    //           </a>
+    //           <div className="rf-header__navbar" style={{ marginBottom: '18px' }}>
+    //             <div className="rf-service">
+    //               <a className="rf-service__title titrage" href="/" title="Portail Conseiller Numérique">
+    //               Votre espace Conseiller Numérique France Services
+    //               </a>
+    //               { role === 'structure' &&
+    //               <p className="rf-service__tagline">Espace de gestion des candidatures</p>
+    //               }
+    //               { role === 'prefet' &&
+    //               <p className="rf-service__tagline">Espace de visualisation des structures</p>
+    //               }
+    //             </div>
+    //           </div>
+    //           { connected &&
+    //           <div className="rf-header__tools" style={{ marginBottom: '33px', display: 'flex' }}>
+    //             <div className="rf-shortcuts">
+    //               <ul className="rf-shortcuts__list">
+    //                 <li className="rf-shortcuts__item">
+    //                   <span>
+    //                     <ul className="rf-nav-list">
+    //                       <li className="rf-nav__item">
+    //                         <button className="rf-sidemenu__btn" onClick={menuClick} aria-expanded={menu} aria-controls="menu-776" aria-current="false">
+    //                           <h3 className="rf-tile__title">
+    //                             <span className="rf-fi-account-fill rf-ml-10v" /> {user?.name}
+    //                           </h3>
+    //                         </button>
+    //                         <div className={ menu === true ? 'rf-collapse--expanded' : 'rf-collapse rf-nav--expanded'} id="menu-776">
+    //                           <ul className="rf-menu__list" style={{ paddingInlineStart: '3rem', marginTop: '1rem' }}>
+    //                             <li>
+    //                               <Link to={'/mon-compte'}>Mon compte</Link>
+    //                             </li>
+    //                           </ul>
+    //                         </div>
+    //                       </li>
+    //                     </ul>
+    //                   </span>
+    //                 </li>
+    //                 <li className="rf-shortcuts__item">
+    //                   <Link className="rf-btn rf-btn--sm" to={`/login?role=${role}`}>Se déconnecter&nbsp;<i className="ri-logout-box-r-line"></i></Link>
+    //                 </li>
+    //               </ul>
+    //             </div>
+    //           </div>
+    //           }
+    //         </div>
+    //       </div>
+    //       <div className="rf-col"></div>
+    //     </div>
+    //   </div>
+    // </header>
   );
 
 }
