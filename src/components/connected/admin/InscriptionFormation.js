e2e90b9f78aade2234ebc1b5842360ebc8cfa14c
Sébastien Quirrion <sebastien@fitlab.fr>
2021-09-13 13:39:25 +0200

    petit ajust wording

--- src/components/connected/admin/InscriptionFormation.js
+++ src/components/connected/admin/InscriptionFormation.js
@@ -5,11 +5,11 @@ function InscriptionFormation() {
   return (
     <div className="formation">
       <div className="rf-container-fluid">
-        <h4>Présentation de l&rsquo;offre de formation des Conseillers numériques France Services</h4>
+        <h4>Présentation de l&rsquo;offre de formation des Conseillers numériques France services</h4>
         <div className="rf-grid-row">
           <a href="https://cdn.conseiller-numerique.gouv.fr/Presentation_offre_de_formation.pdf"
             className="rf-link" target="blank" rel="noreferrer" title="Offre de formation">
-              Télécharger la présentation de l&rsquo;offre de formation (slides au format PDF)
+              Télécharger la présentation de l&rsquo;offre de formation (format PDF)
           </a>
         </div>
       </div>
