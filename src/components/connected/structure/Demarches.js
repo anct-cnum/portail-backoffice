d74b81265c2367de0b55a73d86ef03d7cb069204
Marc Gavanier <marc.gavanier@gmail.com>
2021-09-20 18:31:36 +0200

    modification de la formulation des textes correspondants à l'onglet mes démarches

--- src/components/connected/structure/Demarches.js
+++ src/components/connected/structure/Demarches.js
@@ -10,50 +10,61 @@ function Demarches() {
             <p>Vous retrouverez ci-dessous le lien vous permettant de déposer votre dossier de demande de subvention via l&rsquo;outil Démarches Simplifiées.
             </p>
             <p>Nous vous informons que les demandes sont traitées en fonction de la date de recrutement et d&rsquo;entrée en formation&nbsp;
-              de votre conseiller numérique France services,&nbsp;
+              de votre Conseiller numérique France Services&nbsp;-
               et non pas selon la date de dépôt de votre dossier.
             </p>
             <p>
             Avant de déposer votre dossier de demande de subvention, vous devez d&rsquo;abord&nbsp;:
               <ul>
                 <li>
-                Sélectionner la date d&rsquo;embauche du ou des candidat(s) retenu(s).
+                Sélectionner la date d&rsquo;embauche du ou des candidat(s) retenu(s)&nbsp;;
                 </li>
                 <li>
-                Arrêter votre choix sur un ou plusieurs conseiller(s) numérique(s) France services
+                Arrêter votre choix sur un ou plusieurs Conseiller(s) numérique(s) France services
                 en cliquant sur le bouton &quot;Valider cette candidature&quot;.
                 </li>
               </ul>
             </p>
             <p><b>Votre demande de subvention ne pourra pas être traitée si ces étapes n&rsquo;ont pas été respectées.</b>
             </p>
+            <p><b>Vous ne devez remplir qu&rsquo;une seule demande quel que soit le nombre de postes que l&rsquo;État a accordé à votre structure.</b>
+            </p>
           </div>
         </div>
         <div className="rf-grid-row">
-          <p>Collectivités, GIP et autres structures publiques :
+          <p>Collectivités, GIP et autres structures publiques&nbsp;:
             <br/>
-            <a href="https://www.demarches-simplifiees.fr/commencer/cnfs-sa-structures-publiques"
+            <a href="https://www.demarches-simplifiees.fr/commencer/sa-cnfs-structures-publiques"
               title="cnfs-sa-structures-publiques"
               className="rf-link"
-              target="blank">https://www.demarches-simplifiees.fr/commencer/cnfs-sa-structures-publiques</a>
+              target="blank">https://www.demarches-simplifiees.fr/commencer/sa-cnfs-structures-publiques</a>
           </p>
         </div>
         <div className="rf-grid-row">
-          <p>Entreprises de l&rsquo;économie sociale et solidaire et autres structures privées :
+          <p>Entreprises de l&rsquo;économie sociale et solidaire&nbsp;et autres structures privées&nbsp;:
             <br/>
-            <a href="https://www.demarches-simplifiees.fr/commencer/cnfs-sa-entreprises"
-              title="cnfs-sa-entreprises"
+            <a href="https://www.demarches-simplifiees.fr/commencer/sa-cnfs-entreprises"
+              title="sa-cnfs-entreprises"
               className="rf-link"
-              target="blank">https://www.demarches-simplifiees.fr/commencer/cnfs-sa-entreprises</a>
+              target="blank">https://www.demarches-simplifiees.fr/commencer/sa-cnfs-entreprises</a>
           </p>
         </div>
         <div className="rf-grid-row">
-          <p>Associations loi de 1901/1908 :
+          <p>Associations loi de 1901/1908&nbsp;:
             <br/>
-            <a href="https://www.demarches-simplifiees.fr/commencer/cnfs-associations"
-              title="cnfs-sa-associations"
+            <a href="https://www.demarches-simplifiees.fr/commencer/sa-cnfs-associations"
+              title="sa-cnfs-associations"
               className="rf-link"
-              target="blank">https://www.demarches-simplifiees.fr/commencer/cnfs-associations</a>
+              target="blank">https://www.demarches-simplifiees.fr/commencer/sa-cnfs-associations</a>
+          </p>
+        </div>
+        <div className="rf-grid-row">
+          <p>N&rsquo;hésitez pas à consulter notre guide « pas-à-pas » pour vous guider dans vos démarches&nbsp;:
+            <br/>
+            <a href="https://cdn.conseiller-numerique.gouv.fr/Guide_pas_a_pas.pdf"
+              title="guide-pas-a-pas"
+              className="rf-link"
+              target="blank">https://cdn.conseiller-numerique.gouv.fr/Guide_pas_a_pas.pdf</a>
           </p>
         </div>
       </div>
