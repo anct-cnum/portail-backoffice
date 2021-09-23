6667a5f20576b6ed93d049a4f648d9bebbaf2723
Jean-Yves Stervinou <jy@stervinou.com>
2021-09-23 17:04:05 +0200

    - Affichage de la liste des candidats validés - Liens vers les pages détails des candidats validés et recrutés

--- src/components/connected/admin/StructureDetails.js
+++ src/components/connected/admin/StructureDetails.js
@@ -314,7 +314,36 @@ function StructureDetails({ location }) {
                      </h3>
                      {stat.candidats && stat.candidats.map((candidat, idx) =>
                        <p key={idx}>
-                         {candidat}
+                         <Link to={{
+                           pathname: `/candidat/${candidat?._id}`,
+                           origin: `/structure/${structure?.structure?._id}` }}>
+                           {candidat.prenom} {candidat.nom}
+                         </Link>
+                       </p>
+                     )}
+                     {!stat.candidats &&
+                      <p>Aucun candidat trouvé</p>
+                     }
+                   </>
+                  }
+                </div>
+              )}
+
+              {stats.map((stat, idx) =>
+                <div key={idx}>
+                  {stat.statut === 'recrutee' &&
+                   <>
+                     <h3 className="capitalizeFirstLetter">
+                       {stat.count > 1 && statutsLabel.find(label => label.key === stat.statut).name }
+                       {stat.count <= 1 && statutsLabel.find(label => label.key === stat.statut).nameSingle }
+                     </h3>
+                     {stat.candidats && stat.candidats.map((candidat, idx) =>
+                       <p key={idx}>
+                         <Link to={{
+                           pathname: `/candidat/${candidat?._id}`,
+                           origin: `/structure/${structure?.structure?._id}` }}>
+                           {candidat.prenom} {candidat.nom}
+                         </Link>
                        </p>
                      )}
                      {!stat.candidats &&
